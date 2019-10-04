006a318993ab43eed0e539262e21643c5fd8b665
msaadat67 <msaadat67@gmail.com>
2019-10-04 05:18:02 -0700

    EmailSender Service Added

---
+++ MFTShop/Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class EmailSender
+{
+	public EmailSender()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
