8a210215807c74a02c9444fb16ceba191397f669
MFT <MFT@DESKTOP-95J8BK0>
2019-10-04 04:53:15 -0700

    Initial creation of project

---
+++ MFTShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MFTShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
