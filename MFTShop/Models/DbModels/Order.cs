b26140053054fb5e3038cdcb227cb65cb9d8cd31
AliMi00 <alimobinidev@gmail.com>
2019-10-07 22:14:21 +0330

    added Order,OrderDetails,Customer Models and DbSet of this Models (No Migration)

---
+++ MFTShop/Models/DbModels/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFTShop.Models.DbModels
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal AmountBuy { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
---
+++ MFTShop/Models/DbModels/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFTShop.Models.DbModels
+{
+    public class OrderDetail
+    {
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+        //Product Price When Product was Sold
+        public decimal UnitPriceBuy { get; set; }
+        public int qty { get; set; }
+        public double discount { get; set; }
+        public double Tax { get; set; }
+        //the time that product added to order
+        public DateTime AddedProductDate { get; set; }
+    }
+}
