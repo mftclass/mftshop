84909e3da21429fabdf71db3e8fd9e5a3aec9d19
majid <msaadat67@gmail.com>
2019-11-25 21:02:42 +0330

    AddProductToOrder method added

--- MFTShop/Controllers/ProductsController.cs
+++ MFTShop/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using MFTShop.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -10,9 +11,12 @@ namespace MFTShop.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductServices productServices;
-        public ProductsController(IProductServices productServices)
+        private readonly IOrderServices orderServices;
+        private string UserName;
+        public ProductsController(IProductServices productServices, IOrderServices orderServices)
         {
             this.productServices = productServices;
+            this.orderServices = orderServices;
         }
         public IActionResult Index()
         {
@@ -24,7 +28,13 @@ namespace MFTShop.Controllers
             return View(products);
 
         }
-         
+        [Produces("application/json")]
+        public IActionResult AddProductToOrder(int productId, int? orderId, int quantity = 1)
+        {
+            UserName = User.FindFirstValue(ClaimTypes.Name);
+            var result = orderServices.saveOrder(UserName, productId, orderId, quantity);
+            return Json(result);
+        }
 
     }
 }
