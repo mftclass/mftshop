23f9bdda758c346af1b1dd9114e8361c6f118b0e
AliTestP <alimobini996@gmail.com>
2019-12-09 21:16:18 +0330

    Add user order report services and controller

---
+++ MFTShop/Controllers/OrdersReportController.cs
@@ -0,0 +1,41 @@
+﻿using MFTShop.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MFTShop.Controllers
+{
+    public class OrdersReportController : Controller
+    {
+        private readonly IOrderReportServices orderReportServices;
+        string Username
+        {
+            get
+            {
+                return User.FindFirstValue(ClaimTypes.Name);
+            }
+        }
+
+        public OrdersReportController(IOrderReportServices orderReportServices)
+        {
+            this.orderReportServices = orderReportServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetUserOrdersReport()
+        {
+            var orders = orderReportServices.CustomerOrders(Username);
+            return Json(orders);
+        }
+        public IActionResult GetUserDetailsReport(int OrderId)
+        {
+            var order = orderReportServices.CustomerOrderDetails(OrderId, Username);
+            return Json(order);
+        }
+    }
+}
---
+++ MFTShop/Services/IOrderReportServices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MFTShop.Models.DbModels;
+using MFTShop.Models.ViewModels;
+
+namespace MFTShop.Services
+{
+    public interface IOrderReportServices
+    {
+        OrderViewModel CustomerOrderDetails(int OrderId, string Username);
+        List<Order> CustomerOrders(string username, int? orderId = null, OrderStatusTypes? status = OrderStatusTypes.Open, bool withIncludes = false);
+    }
+}
